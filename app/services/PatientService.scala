c942013d8d14c212477eb19ab87b75b0caf00f1b
phakama <ntshewulap@gmail.com>
2014-09-25 12:10:46 +0200

    Patient service updated

--- app/services/PatientService.scala
+++ app/services/PatientService.scala
@@ -1,7 +1,8 @@
 package services
 
-import domain.{Diagnosis, Patient}
+import domain.{CarePlan, Diagnosis, Patient}
 import repository.CarePlanModel.CarePlanRepo
+import repository.DailyReportModel.DailyReportRepo
 import repository.DiagnosisModel.DiagnosisRepo
 import repository.PatientModel.PatientRepo
 
@@ -9,8 +10,9 @@ import repository.PatientModel.PatientRepo
  * Created by phakama on 2014/09/23.
  */
 trait PatientService {
-  def addPatient(patient : Patient)
-  def getDiagnosis(id : Long) : List[DiagnosisRepo#TableElementType]
-  def displayCarePlan(id : Long) : List[CarePlanRepo#TableElementType]
+  def addPatient(patient : Patient) : Long
+ // def getPatient(id : Long) : List[PatientRepo#TableElementType]
+  def getDiagnosis(id : Long) : Diagnosis
+  def displayCarePlan(id : Long) : CarePlan
 
 }
